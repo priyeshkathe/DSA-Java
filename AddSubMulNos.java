72b11ee9a23fa53283ace96fce02bdc83040e330
Priyesh Kathe <priyeshkathe@gmail.com>
2026-01-12 19:23:08 +0530

    1

--- AddSubMulNos.java
+++ AddSubMulNos.java
@@ -1,5 +1,6 @@
+// Addition, Subtractoin,Multiplication of 2 Numbers
 import java.util.*;
-public class SumOfTwoNumbers {  
+public class AddSubMulNos {  
     public static void main (String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter 1st Number :");
@@ -8,6 +9,11 @@ public class SumOfTwoNumbers {
         int b = sc.nextInt();
 
         int c = a+b;
+        int d = a-b;
+        int e = a*b;
+
         System.out.println("Addition of Numbers is : "+c);
+        System.out.println("Subtraction of Numbers is : "+d);
+        System.out.println("Multiplication of Numbers is : "+e);
     }
 }
